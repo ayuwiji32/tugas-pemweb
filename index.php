91ff7cffe6e3a7024000effd58bca396b253bd9c
ayuwiji32 <46211478+ayuwiji32@users.noreply.github.com>
2019-04-20 17:54:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
-
 <!DOCTYPE html>
 <html>
 <head>
 
 </head>
 <body>
+	
 <?php
-    // mulai session
-    session_start();
+    
     // cek keberadaan cookie 'username'
     // jika ada, maka status = true. jika tak ada, maka status = false
     setcookie('username', '', time()+1);
@@ -17,14 +16,15 @@
         $status = false;
     }
 ?>
+
 </body>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Game CrazyMath</title>
+	<title>Game CrazyMath</title> 
 </head>
 <body>
-	<h1>Crazy Math</h1>
+	<h1><center><font face="wide latin"> Crazy Math</font></center> </h1><br>
 	
     <form enctype="multipart/form-data" method="post" action="action.php">
         <?php
@@ -32,10 +32,10 @@
             // dan tombol submit dg nama 'submit1'
             if ($status == false){
         ?>
-            Masukkan nama Anda : <input type="text" name="username"><br>
+             <center><font face="allegro bt"> Masukkan nama Anda : <input type="text" name="username"> <br><br>
 		
-            Masukkan foto Anda   : <input  type="file" name="userfile"/><br>
-            <input type="submit" name="submit1" value="Start !!">
+            Masukkan foto profil Anda   : <input  type="file" name="userfile"/><br><br>
+            <input type="submit" name="submit1" value="MULAI !!"></font></center>
         <?php       
             
 
@@ -43,21 +43,33 @@
 			// jika status = true (cookie username ada), maka tampilkan username
 			// tanggal terakhir kali main, dan score. Data ini diambil dari cookie
 			// serta tampilkan tombol submit dg nama 'submit2'	
-			echo "<p>Welcome back, ".$_COOKIE['username']."</p>";
-			echo "<p>Sebelumnya, terakhir kali Anda main game ini tanggal ".$_COOKIE['lasttime']." dengan score ".$_COOKIE['score']."</p>";	
-			echo "<a href='index.php' name='status' value='false'>Not you?</a>"; 
-            
+			echo "<p> Welcome back, ".$_COOKIE['username']."</p>";
+			echo "<p>Sebelumnya Anda terakhir kali main game ini tanggal ".$_COOKIE['lasttime']." dengan score ".$_COOKIE['score']."</p>";	
+			 
+           
         ?>
-            <input type="submit" name="submit2" value="Start !!">
+
+        <form method="post" class="button" action="action.php">
+            <input type="submit" name="submit1" value="MULAI LAGI !!">
+        </form>
+        <?php
+        
+        echo '<p>Bukan ' . $_COOKIE["username"] . '? <br/>Klik <a href="hapuscookie.php">disini</a></p>';
+   
+        ?>
+
         <?php       
             }
         ?>
         
     </form>
     <?php
+    // mulai session
+    session_start();
         // nilai inisialisasi lives dan score (simpan ke session)
         $_SESSION['lives'] = 5;
         $_SESSION['score'] = 0;
     ?>
+
 </body>
 </html>
